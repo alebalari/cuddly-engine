b2486072e01d860f7b14f3e92fbd3d13751fb6f6
Alejandro Balari <alejandro@deployblackbox.com>
2022-12-07 14:45:54 -0500

    morgan logs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello World');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import app from './app';
-import morgan from 'morgan';
 
 const port = process.env.PORT != null || 3001;
-
-app.use(morgan('dev'));
 
 app.listen(port, () => {
 	console.log(`Example app is listening on port ${port}`);
